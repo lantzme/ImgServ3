f1b722ba36343aa9fdb603ea433efc1099e06f3b
lantzme <eyal.lantz@gmail.com>
2018-05-28 02:40:09 +0300

    Add files via upload

---
+++ ImageService.Infrastructure/Enums/CommandEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Infrastructure.Enums
+{
+    public enum CommandEnum
+    {
+        NewFileCommand,
+        GetConfigCommand,
+        GetLogsCommand,
+        NewLog,
+        LogCommand,
+        CloseCommand,
+        CloseDirectoryHandlerCommand,
+        CloseClientConnection
+    }
+}
